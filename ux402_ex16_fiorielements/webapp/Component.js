f801e411f79a1eaba6aee5e00ba7f2a1705fc590
ux410-a02 UX410-A02 <p003778>
2025-01-16 01:42:19 +0000

    add ex16,17 code

---
+++ ux402_ex16_fiorielements/webapp/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("student00.com.sap.training.ux402.fiorielements.ux402fiorielements.Component", {
+            metadata: {
+                manifest: "json"
+            }
+            
+        });
+    }
+);
